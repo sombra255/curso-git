58f2d39b9c0b4d2afa8c7b10cef26fa6e0b29d72
Ramires <fabricio.tidir@gmail.com>
2018-06-28 18:54:51 -0300

    inicio do projeto organizze

---
+++ curso_udemy/CursoUdemyOrganizze/app/src/main/java/br/com/fabricio/cursoudemyorganizze/activity/MainActivity.java
@@ -0,0 +1,62 @@
+package br.com.fabricio.cursoudemyorganizze.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+import br.com.fabricio.cursoudemyorganizze.R;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .canGoForward(false)
+                .build()
+        );
+    }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+}
