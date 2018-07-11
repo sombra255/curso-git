e056965ca3f63cea3a7ab3be26a0f5eec0f5ff76
Ramires <fabricio.tidir@gmail.com>
2018-07-11 16:56:18 -0300

    inicio do projeto whatsapp

---
+++ curso_udemy/WhatsappCloneCursoUdemy/app/src/main/java/br/com/fabricio/whatsappclonecursoudemy/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package br.com.fabricio.whatsappclonecursoudemy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fabricio.whatsappclonecursoudemy.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ curso_udemy/WhatsappCloneCursoUdemy/app/src/main/java/br/com/fabricio/whatsappclonecursoudemy/activity/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.fabricio.whatsappclonecursoudemy.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.fabricio.whatsappclonecursoudemy.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
